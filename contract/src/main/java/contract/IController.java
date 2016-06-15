632f6ff58de60bf3a80230086bfccd8b7a745229
Dylan Peltre <Dylan@10.131.50.10>
2016-06-15 15:59:00 +0200

    controller ajustement

--- contract/src/main/java/contract/IController.java
+++ contract/src/main/java/contract/IController.java
@@ -1,22 +1,7 @@
 package contract;
 
-/**
- * The Interface IController.
- *
- * @author Jean-Aymeric Diet
- */
+
 public interface IController {
 
-	/**
-	 * Control.
-	 */
-	public void control();
-
-	/**
-	 * Order perform.
-	 *
-	 * @param controllerOrder
-	 *          the controller order
-	 */
-	public void orderPerform(ControllerOrder controllerOrder);
+	public void orderDirection (PossibleMove possibleMouv);
 }
--- controller/src/main/java/controller/Controller.java
+++ controller/src/main/java/controller/Controller.java
@@ -1,85 +1,52 @@
 package controller;
 
-import contract.ControllerOrder;
 import contract.IController;
 import contract.IModel;
 import contract.IView;
+import contract.PossibleMove;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class Controller.
- */
-public class Controller implements IController {
-
-	/** The view. */
-	private IView		view;
-
-	/** The model. */
-	private IModel	model;
-
-	/**
-	 * Instantiates a new controller.
-	 *
-	 * @param view
-	 *          the view
-	 * @param model
-	 *          the model
-	 */
+public class Controller {
+	private IView view;
+	private IModel model;
+	
 	public Controller(final IView view, final IModel model) {
 		this.setView(view);
 		this.setModel(model);
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see contract.IController#control()
-	 */
-	public void control() {
-		this.view.printMessage("Appuyer sur les touches 'E', 'F', 'D' ou 'I', pour afficher Hello world dans la langue d votre choix.");
-	}
-
-	/**
-	 * Sets the view.
-	 *
-	 * @param view
-	 *          the new view
-	 */
+	
 	private void setView(final IView view) {
 		this.view = view;
 	}
-
-	/**
-	 * Sets the model.
-	 *
-	 * @param model
-	 *          the new model
-	 */
+	
 	private void setModel(final IModel model) {
 		this.model = model;
 	}
-
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IController#orderPerform(contract.ControllerOrder)
-	 */
-	public void orderPerform(final ControllerOrder controllerOrder) {
-		switch (controllerOrder) {
-			case English:
-				this.model.loadMessage("GB");
+	
+	public void orderDirection (PossibleMove possibleMove){
+		switch(possibleMove){
+			case LEFT:
+				this.model.
 				break;
-			case Francais:
-				this.model.loadMessage("FR");
+			case RIGHT:
+				this.model 
 				break;
-			case Deutsch:
-				this.model.loadMessage("DE");
+			case UP:
+				this.model 
 				break;
-			case Indonesia:
-				this.model.loadMessage("ID");
+			case DOWN:
+				this.model
 				break;
-
-			default:
+			case UPRIGHT:
+				this.model 
+				break;
+			case UPLEFT:
+				this.model 
+				break;
+			case DOWNRIGHT:
+				this.model 
+				break;
+			case DOWNLEFT:
+				this.model
 				break;
 		}
 	}
