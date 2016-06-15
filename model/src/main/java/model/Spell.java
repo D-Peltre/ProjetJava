9e2f2713181d882d65a53868b43e7f1d4674e2b7
Romain <romain.verhaeghe@viacesi.fr>
2016-06-15 15:52:43 +0200

    les sprites sont implémentés

--- model/src/main/java/model/Spell.java
+++ model/src/main/java/model/Spell.java
@@ -1,8 +1,22 @@
 package model;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
 
 public class Spell extends Affichable{
-	
+	ArrayList <Image> sprites;
 	public Spell(int x, int y){
 		super(x,y);
+		try {
+			sprites.add(ImageIO.read(new File("Sprite/fireball_1.png")));
+			sprites.add(ImageIO.read(new File("Sprite/fireball_2.png")));
+			sprites.add(ImageIO.read(new File("Sprite/fireball_3.png")));
+			sprites.add(ImageIO.read(new File("Sprite/fireball_4.png")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
--- model/src/main/java/model/VerticalWall.java
+++ model/src/main/java/model/VerticalWall.java
@@ -1,7 +1,17 @@
 package model;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class VerticalWall extends Affichable{
+	Image sprite;
 	public VerticalWall(int x, int y){
 		super(x,y);
+		try {
+			this.sprite = ImageIO.read(new File("Sprite/vertical_bone.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
