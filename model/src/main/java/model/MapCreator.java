18500ab229aaf4c5758c1c35c480059f0bf6958d
Romain <romain.verhaeghe@viacesi.fr>
2016-06-17 09:13:31 +0200

    asoz

--- model/src/main/java/model/MapCreator.java
+++ model/src/main/java/model/MapCreator.java
@@ -13,32 +13,40 @@ import contract.IAffichable;
 public class MapCreator extends DBConnection{
 	//Create and initialize the String that will hold the map
 	String map = "";
-
+	// Create the list that will hold all objects
 	ArrayList<IAffichable> al;
-	
+	// Constructor of MapCreator, connect to the DB and get the String of the map thanks to a stored procedure
 	public MapCreator() throws SQLException{
+		// idMap is the number associated of a map in our DB
 		String idMap = "1";
+		// Store the name of the stored procedure with its parameters in String sql
 		String sql = "{call getMap(?)}";
 		CallableStatement call = connection.prepareCall(sql);
+		// Put the parameters in the procedure
 		call.setString(1, idMap);
+		// Execute the procedure
 		call.execute();
+		// result of the procedure is stored in resultSet
 		ResultSet resultSet = call.getResultSet();
+		// Select the good line (even if there is only one, it needs to be done)
 		resultSet.next();
+		// store the result in the map variable
 		this.map = resultSet.getString(1);
-		// Recupere le string de la bdd
+		// Close the DB
 		resultSet.close();
 	}
 		
-	
+	//read the map and create object corresponding, stock all of them in al
 	public void createObjects(){
-		//read the map and create object corresponding, stock all of them in al
+		// Allows us to store all objects in this local variable
 		ArrayList <IAffichable> al = new ArrayList<IAffichable>();
+		// get the number of char in the map string and store it in nbChar
 		int nbChar = map.length();
 		int y = 0;
 		int x=0;
-		/// i est l'indice du pointeur vers le map. X et Y sont les coordonnées du nouvel object a créer
+		// create a loop that will increments i from 0 to nbChar-1
 		for(int i = 0;i<nbChar; i++){
-			System.out.print(map.charAt(x));
+			
 			switch(map.charAt(i)){
 				case '-' : al.add(new HorizontalWall(x, y));
 					break;
