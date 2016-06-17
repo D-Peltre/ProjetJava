5f75f059afc181afbb9c9d8be137203655ea9150
Dylan Peltre <Dylan@10.131.50.10>
2016-06-17 14:05:27 +0200

    Boolean etat dans door

--- model/src/main/java/model/Door.java
+++ model/src/main/java/model/Door.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Door extends Affichable{
+	boolean etat = false; //quand la porte est fermé
+	
 	public Door(int x, int y) {
 		super(x,y);
 		try {
@@ -13,9 +15,27 @@ public class Door extends Affichable{
 			e.printStackTrace();
 		}
 	}
+	
 	public String get_type(){
 		return "{";
 	}
 	public void move(){}
+	
+	public boolean getEtat(){
+		return this.etat;
+	}
+	
+	public void setEtat(boolean b){
+		this.etat = b;
+	}
+	
+	public void changeDoor(){
+		try {
+			this.sprite = ImageIO.read(this.getClass().getResourceAsStream("/Sprite/gate_open.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
