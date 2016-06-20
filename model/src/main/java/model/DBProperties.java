985252a1944be957083be1af8587e589b58c5d54
Dylan Peltre <Dylan@10.131.50.10>
2016-06-20 09:53:45 +0200

    Commentaire DBProperties

--- model/src/main/java/model/DBProperties.java
+++ model/src/main/java/model/DBProperties.java
@@ -4,34 +4,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-/**
- * The Class DBProperties.
- *
- * @author Jean-Aymeric Diet
- */
+//Cette classe a pour role de lire le fichier model.properties et de renvoyer l'url, le login et le password qui sont dedans 
 class DBProperties extends Properties {
 
-	/** The Constant serialVersionUID. */
-	private static final long		serialVersionUID			= 5289057445894568927L;
+	private static final long serialVersionUID = 5289057445894568927L;
 
 	/** The Constant PROPERTIES_FILE_NAME. */
-	private final static String	PROPERTIES_FILE_NAME	= "model.properties";
+	private final static String PROPERTIES_FILE_NAME = "model.properties";
 
 	/** The url. */
-	private String							url										= "";
+	private String url = "";
 
 	/** The login. */
-	private String							login									= "";
+	private String login = "";
 
 	/** The password. */
-	private String							password							= "";
+	private String password = "";
 
 	/**
 	 * Instantiates a new DB properties.
 	 */
 	public DBProperties() {
 		InputStream inputStream;
-
+		// imputStream est un flux d'entree qui permet de lire des données, ici
+		// dans le fichier model.properties
 		inputStream = this.getClass().getClassLoader().getResourceAsStream(DBProperties.PROPERTIES_FILE_NAME);
 
 		if (inputStream != null) {
@@ -40,6 +36,8 @@ class DBProperties extends Properties {
 			} catch (final IOException e) {
 				e.printStackTrace();
 			}
+			// Si on peut lire le fichier, on recupere l'url, le login et le
+			// password
 			this.setUrl(this.getProperty("url"));
 			this.setLogin(this.getProperty("login"));
 			this.setPassword(this.getProperty("password"));
@@ -59,7 +57,7 @@ class DBProperties extends Properties {
 	 * Sets the url.
 	 *
 	 * @param url
-	 *          the new url
+	 *            the new url
 	 */
 	private void setUrl(final String url) {
 		this.url = url;
@@ -78,7 +76,7 @@ class DBProperties extends Properties {
 	 * Sets the login.
 	 *
 	 * @param login
-	 *          the new login
+	 *            the new login
 	 */
 	private void setLogin(final String login) {
 		this.login = login;
@@ -97,7 +95,7 @@ class DBProperties extends Properties {
 	 * Sets the password.
 	 *
 	 * @param password
-	 *          the new password
+	 *            the new password
 	 */
 	private void setPassword(final String password) {
 		this.password = password;
