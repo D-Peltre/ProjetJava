b8e08f5313a6673d15d0029c5e298cf8b5dccaa7
Yvar-deGoffau <yvar.degoffau@viacesi.fr>
2016-06-20 10:33:42 +0200

    Javadoc ViewBuilder

--- view/src/main/java/view/ViewBuilder.java
+++ view/src/main/java/view/ViewBuilder.java
@@ -7,26 +7,25 @@ import java.util.ArrayList;
 
 import contract.IAffichable;
 
-/****
-- = mur horizontal
-| = mur vertical
-o = mur diagonal
-{ = porte
-* = bulle d'énergie
-d = démon
-$ = bourse
-L = Lorann
-****/
 
 public class ViewBuilder{
    private ArrayList <IAffichable> objects;
    private ViewPanel viewpanel;
    private int score=0;
+   /**
+    * The ViewBuilder.
+    * This class is responsable for the rendering of the different objects.
+    * @param viewpanel - the ViewPanel who creates it.
+    */
    public ViewBuilder(ViewPanel viewpanel) {
 	 this.viewpanel=viewpanel;
      }
-   public void update(ArrayList <IAffichable> object) {	 
-	   this.objects=object;
+   /**
+    * This function takes the list of objects and fire the drawing procedure of it's parent viewpanel, if possible.
+    * @param objectlist - The list of IAffichables that is going to get shown
+    */
+   public void update(ArrayList <IAffichable> objectlist) {	 
+	   this.objects=objectlist;
 	   try{
 		   this.viewpanel.repaint();
 	   	   }
@@ -34,12 +33,24 @@ public class ViewBuilder{
 		   System.err.println("Unable to get grip on my viewpanel");
 	   	   }
        }
+   /**
+    * This function returns the current score
+    * @return the current score
+    */
    public int get_score(){
 	   return this.score;
    	   }
+   /**
+    * This function return the current list of objects
+    * @return the current list of objects
+    */
    public ArrayList <IAffichable> get_objects(){
 	   return this.objects;
        }
+   /**
+    * This function, as called by the viewpanel, is responsable of blitting the list of objects to the screen.
+    * @param graphics - the graphics context (i.e. the parm of the ViewPanel.paintComponent
+    */
    public void render(Graphics graphics){
 	 graphics.clearRect(0, 0, this.viewpanel.getWidth(), this.viewpanel.getHeight());
 	 graphics.setColor(new Color(0,0,0));
@@ -50,6 +61,10 @@ public class ViewBuilder{
 		   }
 	 graphics.drawString("Score: "+this.score, 10, this.viewpanel.getHeight()-8);
 	 }
+   /**
+    * This function register the score to draw
+    * @param score - the score to draw
+    */
    public void send_score(int score) {
      this.score=score;
      }
