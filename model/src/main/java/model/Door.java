dfaf9fd4022540b6f1f99cf6d2523cd23443d453
Yvar-deGoffau <yvar.degoffau@viacesi.fr>
2016-06-20 15:52:38 +0200

    doc door

--- model/src/main/java/model/Door.java
+++ model/src/main/java/model/Door.java
@@ -3,8 +3,12 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Door extends Affichable{
-	boolean etat = false; //quand la porte est fermé
-	
+	boolean etat = false;
+	/**
+	 * This is a classic door. It is open when the player has the energy, and closed otherwise
+	 * @param x - The X position
+	 * @param y - The Y position
+	 */
 	public Door(int x, int y) {
 		super(x,y);
 		try {
@@ -14,26 +18,47 @@ public class Door extends Affichable{
 		}
 	}
 	
+	/**
+	 * The type of the object ("{")
+	 * @return the type of the object ("{")
+	 */
 	public String get_type(){
 		return "{";
 	}
 	
+	/**
+	 * The update function; checks if Lorann has energy, and if it has, it change state
+	 */
 	public void move(){
 		if(this.world.get_lorann().has_energy()){
 			this.changeDoor();
 		}
 	}
 	
+	/**
+	 * This function returns the current state (open or closed)
+	 * @return 
+	 *  - true if open
+	 *  - false if closed
+	 */
 	public boolean getEtat(){
 		return this.etat;
 	}
 	
+	/**
+	 * This function set the state of the door
+	 * @param b - the new state of the door
+	 */
 	public void setEtat(boolean b){
 		this.etat = b;
 	}
 	
+	/**
+	 * This function opens the door
+	 */
 	public void changeDoor(){
 		try {
+			this.setEtat(true);
 			this.sprite = ImageIO.read(this.getClass().getResourceAsStream("/Sprite/gate_open.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
