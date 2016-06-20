22441f3f4a10a2d0146b6bfda0944d794e9d4df3
Dylan Peltre <Dylan@10.131.50.10>
2016-06-20 09:32:39 +0200

    Commentaire main

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -5,25 +5,22 @@ import controller.Controller;
 import model.ModelManager;
 import view.View;
 
-/**
- * The Class Main.
- ***
- * @author Jean-Aymeric Diet
- */
+
 public abstract class Main {
 
-	/**
-	 * The main method.
-	 *
-	 * @param args
-	 *          the arguments
-	 */
+
 	public static void main(final String[] args) {
-		final IModelManager model = new ModelManager();
+		// On crée notre model
+		final ModelManager model = new ModelManager();
+		// on cree notre vue 
 		final View view = new View(model);
+		// on cree notre controller en lui passant notre vue et notre model
 		final Controller controller = new Controller(view, model);
+		//on appel la méthode setController qui est dans notre vue, elle montre a la vue qui est le controller
 		view.setController(controller);
+		//on appel la méthode setController qui est dans notre vue, elle fait apparaitre la fenetre 
 		view.run();
+		//On fait appel a la méthode control qui est celle qui contient la boucle de notre programme
 		controller.control();
 	}
 }
