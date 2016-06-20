29ab5fc1a7532fc3009f0373e80b3cff4619c55b
Yvar-deGoffau <yvar.degoffau@viacesi.fr>
2016-06-20 10:59:35 +0200

    JavaDoc SoundEffect

--- model/src/main/java/model/SoundEffect.java
+++ model/src/main/java/model/SoundEffect.java
@@ -2,19 +2,26 @@ package model;
 import java.io.*;
 import javax.sound.sampled.*;
 public enum SoundEffect {
+	/* The different resources... sounds generated with <a href="http://www.bfxr.net/">BFXR</a> */
    COIN("/Sound/coin.wav"),
    ENERGY("/Sound/energy.wav"),
    SHOOT("/Sound/shoot.wav"),
    GAMEOVER("/Sound/gameover.wav"),
    BONK("/Sound/bonk.wav");
-   
+    /* The current Audio clip */
    private Clip clip;
    
+    /**
+     * The initializer for a given sound effect
+     * @param soundFileName - the file to load
+     */
    SoundEffect(String soundFileName) {
       try {
-         //URL url = this.getClass().getResource(soundFileName);
+    	   /* get the right ressource from the JAR */
     	 InputStream ressource = this.getClass().getResourceAsStream(soundFileName);
+    	   /* makes a inputstream from it*/
          AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(ressource);
+           /* get a channel, and load our sound effect inside */
          clip = AudioSystem.getClip();
          clip.open(audioInputStream);
       } catch (UnsupportedAudioFileException e) {
@@ -26,13 +33,22 @@ public enum SoundEffect {
       }
    }
    
+   /**
+    * This function starts the given sound effect
+    */
    public void play() {
+	       /* test if it is running */
          if (clip.isRunning())
             clip.stop();
+           /* reset the audio pos */
          clip.setFramePosition(0);
+           /* start the effect */
          clip.start();
       	}
    
+   /**
+    * This starts the sound system
+    */
    static void init() {
       values();
    }
